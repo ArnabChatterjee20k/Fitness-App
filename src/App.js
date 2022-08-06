676208ac899b78b659b9f4e0d12298bd4931c7fd
Arnab <arnabchatterjee.ac.2@gmail.com>
2022-08-06 13:25:21 +0530

    added basic structure to our app

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
 import React from 'react'
+import { Route , Routes } from 'react-router-dom'
+import "./App.css"
+import { Box } from '@mui/system'
+import Home from './pages/Home'
+import ExerciseDetail from './pages/ExerciseDetail'
+import Navbar from './components/Navbar'
+import Footer from './components/Footer'
 
 export const App = () => {
   return (
-    <div>App</div>
+    <Box width="400px" sx={{width:{xl:"1488px"}}} m="auto">
+      <Navbar/>
+      <Routes>
+        <Route path="/" element={<Home />}/>
+        <Route path="/exercises/:id" element={<ExerciseDetail />}/>
+      </Routes>
+      <Footer/>
+    </Box>
   )
 }
