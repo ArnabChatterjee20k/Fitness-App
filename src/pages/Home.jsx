4a6787ca7d102285d4a750a3e78fa6e543355487
Arnab <arnabchatterjee.ac.2@gmail.com>
2022-08-07 16:14:22 +0530

    Added states bodypart and exercise in Home component for prop drilling.

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,11 +5,20 @@ import HeroBanner from '../components/HeroBanner'
 import Exercises from '../components/Exercises'
 
 const Home = () => {
+  const [exercises, setExercises] = useState([]);
+  const [bodyPart, setBodyPart] = useState([]);
   return (
     <Box>
-      <HeroBanner/>
-      <SearchExercises/>
-      <Exercises/>
+      <HeroBanner />
+      <SearchExercises
+        setExercises={setExercises}
+        bodyPart={bodyPart}
+        setBodyPart={setBodyPart} />
+      <Exercises
+        setExercises={setExercises}
+        bodyPart={bodyPart}
+        setBodyPart={setBodyPart} />
+
     </Box>
   )
 }
