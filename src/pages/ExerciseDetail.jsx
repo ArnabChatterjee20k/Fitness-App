35ab2b74e8e7d56592072223851875ff8674185b
Arnab <arnabchatterjee.ac.2@gmail.com>
2022-08-07 22:28:51 +0530

    Added details into ExerciseDetail

--- src/pages/ExerciseDetail.jsx
+++ src/pages/ExerciseDetail.jsx
@@ -1,12 +1,31 @@
 import { Box } from '@mui/system'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
 import Details from '../components/Details'
 import ExerciseVideos from '../components/ExerciseVideos'
 import SimilarExercises from '../components/SimilarExercises'
-
+import {fetchData,exerciseOptions} from "../utils/fetchData"
 const ExerciseDetail = () => {
+  const [exerciseDetail, setExerciseDetail] = useState({});
+  const {id} = useParams();
+
+  useEffect(()=>{
+    const fetchExercisesData = async()=>{
+      const exerciseDBURL = `https://exercisedb.p.rapidapi.com`;
+      const youtubeSearchUrl = `https://youtube-search-and-download.p.rapidapi.com`
+
+      const exerciseDetailData = await fetchData(`${exerciseDBURL}/exercises/exercise/${id}`,exerciseOptions)
+      setExerciseDetail(exerciseDetailData);
+
+    }
+    fetchExercisesData();
+  },[id])
   return (
-    <Box>ExerciseDetail</Box>
+    <Box>
+      <Details exerciseDetail={exerciseDetail}/>
+      <ExerciseVideos/>
+      <SimilarExercises/>
+    </Box>
   )
 }
 
