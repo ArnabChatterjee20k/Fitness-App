234cc4ab07c435715e5138a46905a33e828c3fd7
Arnab <arnabchatterjee.ac.2@gmail.com>
2022-08-08 23:00:57 +0530

    added exercise videos

--- src/components/VideoCards.jsx
+++ src/components/VideoCards.jsx
@@ -10,7 +10,7 @@ import React from 'react'
 export const VideoCards = ({ channelName, time, image, link, description }) => {
     console.log(channelName)
     return (
-        <Card sx={{maxWidth:"400px",maxHeight:"400px",textDecoration:"none"}} component="a" href={link} target="_blank">
+        <Card elevation={1} sx={{maxWidth:"400px",maxHeight:"400px",textDecoration:"none"}} component="a" href={link} target="_blank">
             <CardHeader
                 avatar={<Avatar>{channelName.charAt(0)}</Avatar>}
                 title={channelName}
