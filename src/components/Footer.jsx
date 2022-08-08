c757b7df979bd26fa0cf375f4025c86fb001c74b
Arnab <arnabchatterjee.ac.2@gmail.com>
2022-08-08 23:55:04 +0530

    Footer ready

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
+import { Box , Stack , Typography } from '@mui/material'
 
+import Logo from "../assets/assets/images/Logo-1.png"
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <Box mg="80px" bgcolor="#fff3f4">
+      <Stack gap="40px" alignItems="center" px="40px" pt="24px">
+        <img src={Logo} alt="logo" width="200px" height="40px" />
+        <Typography variant="h5" pt="20px" mt="10px">
+          Made with ❤ by Arnab Chatterjee
+        </Typography>
+      </Stack>
+    </Box>
   )
 }
 
